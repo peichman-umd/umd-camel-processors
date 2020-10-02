5820e0570743e512b8072a1a795345c1c0df2aa7
Peter Eichman <peichman@umd.edu>
2020-10-02 09:09:25 -0400

    LIBFCREPO-894. added unit test for LdpathProcessor
    https://issues.umd.edu/browse/LIBFCREPO-894

--- src/main/java/edu/umd/lib/camel/processors/LdpathProcessor.java
+++ src/main/java/edu/umd/lib/camel/processors/LdpathProcessor.java
@@ -1,41 +1,76 @@
 package edu.umd.lib.camel.processors;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.marmotta.ldpath.LDPath;
 import org.apache.marmotta.ldpath.backend.linkeddata.LDCacheBackend;
+import org.apache.marmotta.ldpath.exception.LDPathParseException;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.URIImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
 import java.io.StringReader;
+import java.util.Collection;
 import java.util.Map;
 
-public class LdpathProcessor implements Processor {
-    private String query;
+public class LdpathProcessor implements Processor, Serializable {
+  private final Logger logger = LoggerFactory.getLogger(LdpathProcessor.class);
 
-    private final ObjectMapper objectMapper;
+  private String query;
 
-    public LdpathProcessor() {
-        this.objectMapper = new ObjectMapper();
+  private final LDPath<Value> ldpath;
+
+  private final ObjectMapper objectMapper;
+
+  public LdpathProcessor() {
+    objectMapper = new ObjectMapper();
+    ldpath = new LDPath<>(new LDCacheBackend());
+  }
+
+  @Override
+  public void process(final Exchange exchange) throws LDPathParseException, JsonProcessingException {
+    final Message in = exchange.getIn();
+    final String uri = in.getHeader("CamelFcrepoUri", String.class);
+    logger.debug("Executing LDPath on {}", uri);
+    logger.trace(query);
+    String jsonResult;
+    try {
+      jsonResult = execute(uri);
+    } catch (LDPathParseException e) {
+      logger.error("LDPath parse error: {}", e.getMessage());
+      throw e;
+    } catch (JsonProcessingException e) {
+      logger.error("JSON processing error: {}", e.getMessage());
+      throw e;
     }
+    assert jsonResult != null;
+    assert !jsonResult.isEmpty();
+    in.setBody(jsonResult);
+    in.setHeader("Content-Type", "application/json");
+  }
 
-    @Override
-    public void process(Exchange exchange) throws Exception {
-        final Message in = exchange.getIn();
-        final String uri = in.getHeader("FcrepoCamelUri", String.class);
-        LDCacheBackend backend = new LDCacheBackend();
-        LDPath ldpath = new LDPath(backend);
-        final Map<String, String> results = ldpath.programQuery(uri, new StringReader(query));
-        final String json = objectMapper.writeValueAsString(results);
-        in.setBody(json);
-        in.setHeader("Content-Type", "application/json");
+  Map<String, Collection<?>> executeQuery(final String uri) throws LDPathParseException {
+    final Map<String, Collection<?>> results = ldpath.programQuery(new URIImpl(uri), new StringReader(query));
+    for (Map.Entry<String, Collection<?>> entry : results.entrySet()) {
+      logger.debug("LDPath result: Key: {} Value: {}", entry.getKey(), entry.getValue());
     }
+    return results;
+  }
 
-    public String getQuery() {
-        return query;
-    }
+  String execute(final String uri) throws LDPathParseException, JsonProcessingException {
+    return objectMapper.writeValueAsString(executeQuery(uri));
+  }
 
-    public void setQuery(String query) {
-        this.query = query;
-    }
+  public String getQuery() {
+    return query;
+  }
+
+  public void setQuery(String query) {
+    this.query = query;
+  }
 }
