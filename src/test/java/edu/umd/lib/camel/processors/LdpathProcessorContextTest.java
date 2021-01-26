ae47870c9e592c6d4bbae1c087130fb8845966ab
David P. Steelman <dsteelma@umd.edu>
2021-01-26 08:27:22 -0500

    LIBFCREPO-949. Refactored LdpathProcessorContext to override methods
    Refactored the "LdpathProcessor" class, placing auth token retrieval
    into a separate method, so that it can be overridden
    by subclasses (such as a "test" subclass that simply returns a string)
    
    Also changed the "getLinkedDataResourceUrl" method to "protected",
    so it can be overridden by a subclass, if needed.
    
    In LdpathProcessorContextTest, enabled the tests to pass successfully by
    adding a "test" version of the "LdpathProcessor" class, and rewriting
    the tests to remove the Spring dependencies.
    
    Removed the Spring dependences from the pom.xml, which were clashing
    with the Spring versions provided by the "umd-fcrepo-webapp" dependency.
    
    https://issues.umd.edu/browse/LIBFCREPO-949

--- src/test/java/edu/umd/lib/camel/processors/LdpathProcessorContextTest.java
+++ src/test/java/edu/umd/lib/camel/processors/LdpathProcessorContextTest.java
@@ -1,21 +1,26 @@
 package edu.umd.lib.camel.processors;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
+import org.openrdf.repository.RepositoryException;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-@ContextConfiguration
-@RunWith(CamelSpringBootRunner.class)
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
 public class LdpathProcessorContextTest {
 
   @EndpointInject(uri = "mock:result")
@@ -29,30 +34,75 @@ public class LdpathProcessorContextTest {
 
   private final String uri = "http://localhost:8080/rest/af/c6/d8/20/afc6d820-427a-4932-9df5-3eb002958fd2";
 
-  @DirtiesContext
-  @Test
-  public void testSimpleLdpathProcessor() throws InterruptedException {
-    resultEndpoint.expectedBodiesReceived("{\"id\":[\"" + uri + "\"]}");
-
-    simpleStart.sendBodyAndHeader("", "CamelFcrepoUri", uri);
-
-    resultEndpoint.assertIsSatisfied();
+  private String getResourceAsString(final String name) throws IOException {
+    final InputStream resource = Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
+    assert resource != null;
+    return IOUtils.toString(resource, Charset.forName("UTF-8"));
+  }
+  
+  /**
+   * Overrides LdpathProcessor to simplify authorization token and linkedDataResourceUrl creation
+   */
+  class TestLdpathProcessor extends LdpathProcessor {
+    public TestLdpathProcessor() throws RepositoryException {
+      super();
+    }
+    
+    @Override
+    protected String getAuthToken(final Exchange exchange, final String issuer) {
+      return "abcd-1234";
+    }
+    
+    @Override
+    protected String getLinkedDataResourceUrl(String authToken, String containerBasedUri) {
+      return containerBasedUri;
+    }
+  }
+  
+  protected LdpathProcessor simpleLdpathProcessor() throws RepositoryException, IOException {
+    LdpathProcessor processor = new TestLdpathProcessor();
+    processor.setQuery(getResourceAsString("simpleProgram.ldpath"));
+    return processor;
   }
 
-  @DirtiesContext
+  protected LdpathProcessor complexLdpathProcessor() throws RepositoryException, IOException {
+    LdpathProcessor processor = new TestLdpathProcessor();
+    processor.setQuery(getResourceAsString("complexProgram.ldpath"));
+    return processor;
+  }
+  
   @Test
-  public void testComplexLdpathProcessor() throws InterruptedException {
-    resultEndpoint.setExpectedCount(1);
+  public void testSimpleLdpathProcessor() throws Exception {
+    CamelContext ctx = new DefaultCamelContext();
+    Exchange exchange = new DefaultExchange(ctx);
+  
+    LdpathProcessor processor = simpleLdpathProcessor();
 
-    complexStart.sendBodyAndHeader("", "CamelFcrepoUri", uri);
+    Message in = exchange.getIn();
+    in.setHeader("CamelFcrepoUri", uri);
+    in.setBody("");
+    processor.process(exchange);
 
-    resultEndpoint.assertIsSatisfied();
+    assertEquals("{\"id\":[\"" + uri + "\"]}", in.getBody());
+  }
 
-    final Exchange exchange = resultEndpoint.getExchanges().get(0);
+  @Test
+  public void testComplexLdpathProcessor() throws Exception {
+    CamelContext ctx = new DefaultCamelContext();
+  
+    Exchange exchange = new DefaultExchange(ctx);
+    
+  
+    LdpathProcessor processor = complexLdpathProcessor();
+    
+    Message in = exchange.getIn();
+    in.setHeader("CamelFcrepoUri", uri);
+    in.setBody("");
+    processor.process(exchange);
+
     final String jsonResult = exchange.getIn().getBody(String.class);
     assertFalse(jsonResult.isEmpty());
     assertTrue(jsonResult.startsWith("{"));
     assertTrue(jsonResult.endsWith("}"));
   }
 }
-
